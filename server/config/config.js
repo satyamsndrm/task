01140a5cde40276cded45e1acd21c71779fd4c95
ogedi <satyamsndrm@gmail.com>
2019-04-13 02:22:54 +0530

    configurations for database added.

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+var options = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "task",
+  connectTimeout: 2147483647,
+  multipleStatements: true
+};
+module.exports = {
+  configObj: options
+};
