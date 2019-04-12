5bd2e006d2cd8fbf1c38d2db7facb0d9ef1ce353
ogedi <satyamsndrm@gmail.com>
2019-04-13 02:25:02 +0530

    index.js file added to handle server req.

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+// const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/student", require("./routes/student"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening at ${PORT}`);
+});
